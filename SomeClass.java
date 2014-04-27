8c4449e0983d4fe164476a635af964bdd847e65b
YaroslV <yaroslav.voznij@gmail.com>
2014-04-27 15:32:13 +0300

    Update SomeClass.java

--- SomeClass.java
+++ SomeClass.java
@@ -1,9 +1,11 @@
 
-
 public class SomeClass {
   private Integer Value;
   public Integer GetValue(){
     return Value;
+  }
+  public void FuncInNewBranch(){
+    System.out.println("This function which was addedin new branch");
   }
   
 }
