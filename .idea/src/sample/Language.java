85456edf57287c6f0e92536ac96f8cd2c6dc80d6
Honcharevskyi Dmytro <dimagonchar7@gmail.com>
2014-05-26 20:42:23 +0300

    Update README.md

---
+++ .idea/src/sample/Language.java
@@ -0,0 +1,18 @@
+package sample;
+
+/**
+ * Created by Yaroslav on 5/22/2014.
+ */
+import java.util.Vector;
+
+public class Language extends CountryDirectory {
+
+    public Vector  myCountryDirectory;
+    public Vector  myYear;
+
+    public void ChangeLanguage() {
+        myYear.add(13);
+
+    }
+
+}
